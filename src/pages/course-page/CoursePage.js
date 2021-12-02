193f78fa7c6fb1148a6df3dffeb22b98bb66e1d1
Shon Salamon <shonsalamon@mail.tau.ac.il>
2021-12-02 22:04:52 +0200

    adding support in upload file to s3 - basic upload

--- src/pages/course-page/CoursePage.js
+++ src/pages/course-page/CoursePage.js
@@ -2,18 +2,30 @@ import ExamFilter from "../../components/exam-filter/ExamFilter";
 import ExamsList from "../../components/exams-list/ExamsList";
 import CourseHeader from "../../components/course-header/CourseHeader";
 import Discussions from "../../components/discussions/Discussions";
-import {getAllCourseDiscussions} from "../../API/coursesApi"
+import PopupFileUploading from "../../components/file-uploading/PopupFileUploading";
 // import { useParams } from "react-router";
 
-const CoursePage = () => {
 
+const CoursePage = ({setIsPopupOpen,isPopupOpen}) => {
+    
     // const IDs = useParams();
-    getAllCourseDiscussions();
+    // useEffect(()=>{
+    //     if(isPopupOpen){
+    //         document.documentElement.classList.add("filter")
+    //         document.documentElement.classList.add("blur-lg")
+    //     } 
+    //     else{
+    //         document.documentElement.classList.remove("filter")
+    //         document.documentElement.classList.remove("blur-lg")
+    //     }
+    //     console.log(document.documentElement)
+    // },[isPopupOpen]);
 
     return (
-        <div dir='rtl' className="flex shadow rounded-lg p-5 flex-col mx-auto my-10 items-center max-w-5xl dark:text-white dark:bg-gray-800">
+
+        <div dir='rtl' className=" flex shadow rounded-lg p-5 flex-col mx-auto my-10 items-center max-w-5xl dark:text-white dark:bg-gray-800 ">
             <div className="w-full flex flex-col">
-                <CourseHeader courseName={'אלגוריתמים'} button1Text={'דיון חדש'} button2Text={'הוסף מבחן'} />
+                <CourseHeader isPopupOpen={isPopupOpen} setIsPopupOpen={setIsPopupOpen} courseName={'אלגוריתמים'} button1Text={'דיון חדש'} button2Text={'הוסף מבחן'} />
                 <Discussions />
             </div>
             <ExamFilter />
