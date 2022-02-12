9fdd1c5df13b750bf4cfa049172db4b4c968839d
Shon Salamon <shonsalamon@mail.tau.ac.il>
2022-02-12 12:15:36 +0200

    fixes logout bug

--- src/general-components/LogoutButton.js
+++ src/general-components/LogoutButton.js
@@ -4,17 +4,11 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 const LogoutButton = () => {
     const { logout } = useAuth0();
-    const { loginWithRedirect } = useAuth0();
-
-    const LogoutFromAccount = () => {
-        logout();
-        loginWithRedirect();
-    }
 
     return (
         <div className='ml-5'>
             <button
-                onClick={() => LogoutFromAccount()}
+                onClick={() => logout({returnTo:window.location.origin})}
                 className="w-full flex  items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">התנתקות</button>
         </div>
     )
